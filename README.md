9101b6421911d17540b91c461a46a824e72eaa6e
Fabian <fabischw135@gmail.com>
2022-09-06 15:32:34 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[EN] This program is currently only available in german, sorry.                                                     
+[DE] Dieses Programm diesnt dazu, (fast) alle deutschen Wörter nach einem oder mehrern substrings zu durchsuchen. Ich besitze keine Rechte an der Wortliste, welche ich im Internet gefunden habe.
+
+-Nutzung generelle Version: Einfach die betreffenden arrays und Variaben im Quellcoe ändern und Programm ausführen
+
+-common configs: Einfach eine common config auswählen und die .py Datei ausführen, Ergebnisse werden in entsprechend benannte Zieldatein gespeichert.
---
+++ common_configs/abiturmotto.py
@@ -0,0 +1,54 @@
+#common config für Abiturmottos, durchsucht alle deutschen Wörter nach substrings 'abi' und 'api', Maximallänge auf 15 gesetzt
+
+#HIER GESUCHTE WÖRTER/BUCHSTABENFOLGEN IN DIE LISTE EINTRAGEN
+searched_word_list = ["abi","Abi","api"]#gesuchte Wörter
+
+#HIER DATEI EINGEBEN, IN WELCHE DIE ERGEBNISSE GESPEICHERT WERDEN
+targetfile = "abi_Ideen.txt"#Detei, in welche Ergebnisse gespeichert werden
+
+#HIER WÖRTER EINGEBEN, DIE HERUASGEFILTERT WERDEN SOLLEN 
+excepted_words = ["Abitur","abitur"]#Worte, welche aussortiert werden sollen
+
+#HIER MAXIMALE LÄNGE EINGEBEN
+max_len = None#Maximale Länge
+if max_len == None:
+    max_len = 10000000
+
+success_count = 0 #Count für Anzahl an Treffern
+
+#Deutsche Wortliste öffnen
+with open("wordlist-german.txt") as file:
+    data = file.readlines()
+
+#Targetfile clearen
+with open(targetfile,"w") as clearfile:
+                clearfile.write("")
+
+for searched_word in searched_word_list:
+
+    #durch Datensatz loopen
+    for element in data:
+        if element.find(str(searched_word)) > -1:
+
+            write = True
+
+            #Prüfen, ob Wort in exception Liste ist
+            for exceptions in excepted_words:
+                if element.find(exceptions) > -1:
+                    print("flagged word: "+str(element))
+                    write = False
+                
+            #Wort in targetfile schreiben
+            if write == True and len(element) < max_len:
+                success_count = success_count + 1
+                print(element)
+                file2 = open(targetfile, "a")
+                file2.write(str(element))
+                file2.close()
+
+    file3 = open(targetfile,"a")
+    file3.write("--------------------------\n")
+    file3.close()
+
+print("-------------------------------\n")
+print("Treffer: "+str(success_count))
